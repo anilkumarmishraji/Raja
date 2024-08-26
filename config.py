844e8023feebbaa74144b20ee138509523860cf6
anilkumarmishraji <anilkumarmishraji80@gmail.com>
2024-08-26 10:22:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,22 +7,22 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
-API_HASH = "API_HASH"
+API_ID = 27345751
+API_HASH = "2f4074e9f5a56e99fd9ba562f2314523"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "BOT_TOKEN"
+BOT_TOKEN = "7222494140:AAG0fcNb_OefTPv2YrbTQcYiBC-thyC_YDQ"
 
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = "MONGO_DB_URI"
+MONGO_DB_URI = "mongodb+srv://hayibop613:Anil1234@cluster0.v2cxmsj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = LOG_GROUP_ID
+LOG_GROUP_ID = -1002185119121
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = OWNER_ID
+OWNER_ID = 7282582221
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
